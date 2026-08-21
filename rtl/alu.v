1d13019d35e841bfe2b88befb3022e78b0a2f271
Sai Krishna <saikrishnaac855@gmail.com>
2026-08-21 10:09:24 +0530

    Fix Day 2 ALU with XOR and NOT

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,17 +1,22 @@
 module alu(
     input [7:0] a,
     input [7:0] b,
-    input [1:0] sel,
+    input [2:0] sel,
     output reg [7:0] y
 );
 
 always @(*) begin
     case (sel)
-        2'b00: y = a + b;
-        2'b01: y = a - b;
-        2'b10: y = a & b;
-        2'b11: y = a | b;
+
+        3'b000: y = a + b;   // ADD
+        3'b001: y = a - b;   // SUB
+        3'b010: y = a & b;   // AND
+        3'b011: y = a | b;   // OR
+        3'b100: y = a ^ b;   // XOR
+        3'b101: y = ~a;      // NOT
+
         default: y = 8'b00000000;
+
     endcase
 end
 
